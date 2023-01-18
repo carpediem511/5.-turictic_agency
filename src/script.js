6aff554d4cb2fb084c1baa7938eff4e573e7ba03
carpediem511 <vampire.0514@gmail.com>
2023-01-18 15:14:37 +0300

    проблемы с иконкой

--- src/script.js
+++ src/script.js
@@ -11,14 +11,14 @@ async function getData() {
     tours.forEach(tour => {
 
         document.getElementById("tours-all").innerHTML += `
-        <div>${tour.id}</div>
+        
         <div class="bg-blue-50 rounded-3xl border-sky-500 border-2 xl:w-1/3">
         <div class="relative">
             <div class="flex justify-center pt-10">
                 <img class="drop-shadow-md border" src="${tour.image}">
             </div>
 
-            <div class="title flex flex-col absolute font-attention xl:mx-20 -my-24">
+            <div class="title flex flex-col absolute font-attention xl:mx-20 -my-28">
                 <div class="text-amber-600 xl:text-5xl">${tour.country}</div>
                 <div class="text-current pb-2 mb-2 xl:text-2xl">${tour.city}</div>
             </div>
@@ -35,12 +35,12 @@ async function getData() {
         
             <div class="flex flex-col pb-10 pt-6 px-6">
                 <div class="flex">
-                    <img class="w-16 h-16" src="/src/images/icon-price.png">
+                    <img src="/images/icon-price.png" class="w-12 h-12">
                     <div class="font-basic text-rose-700 pt-6  pl-2 xl:text-3xl font-bold">${tour.price}</div>
                 </div>
 
                 <div class="flex">
-                    <img class="w-12 h-12" src="/src/images/icon-rating.png">
+                    <img src="/images/icon-rating.png" class="w-12 h-12">
                     <div class="font-basic text-amber-400 pt-4 pl-6 xl:text-2xl font-medium">${tour.rating}</div>
                 </div>
             </div>
