76938986fe022c3d0bae74f6caf0547ffec36766
carpediem511 <vampire.0514@gmail.com>
2023-01-18 19:26:54 +0300

    готов css

--- src/script.js
+++ src/script.js
@@ -12,42 +12,46 @@ async function getData() {
 
         document.getElementById("tours-all").innerHTML += `
         
-        <div class="bg-blue-50 rounded-3xl border-sky-500 border-2 xl:w-1/3">
-        <div class="relative">
-            <div class="flex justify-center pt-10">
-                <img class="drop-shadow-md border" src="${tour.image}">
-            </div>
-
-            <div class="title flex flex-col absolute font-attention xl:mx-20 -my-28">
-                <div class="text-amber-600 xl:text-5xl">${tour.country}</div>
-                <div class="text-current pb-2 mb-2 xl:text-2xl">${tour.city}</div>
-            </div>
-        </div>
-
-        <div class="flex flex-col info border drop-shadow-lg xl:mx-20 my-10">
-        
-            <div class="font-basic text-sky-600 text-center font-medium xl:text-2xl pt-10 pb-6">${tour.hotelName}</div>
-        
-            <div class="pl-6">
-                <div class="font-basic text-current pb-2 xl:text-xl pt-10">${tour.startTime}</div>
-                <div class="font-basic text-current xl:text-xl pb-10">${tour.endTime}</div>
-            </div>
-        
-            <div class="flex flex-col pb-10 pt-6 px-6">
-                <div class="flex">
-                    <img src="/images/icon-price.png" class="w-12 h-12">
-                    <div class="font-basic text-rose-700 pt-6  pl-2 xl:text-3xl font-bold">${tour.price}</div>
+        <div class="tour bg-blue-50 rounded-3xl border-sky-500 border-2 max-w-md xl:w-1/4 mx-10 my-10">
+            <div>
+                <div class="flex justify-center pt-6 w-full img">
+                    <img class="border px-8" src="${tour.image}">
                 </div>
 
-                <div class="flex">
-                    <img src="/images/icon-rating.png" class="w-12 h-12">
-                    <div class="font-basic text-amber-400 pt-4 pl-6 xl:text-2xl font-medium">${tour.rating}</div>
+                <div class="title flex flex-col absolute font-attention w-7/12">
+                    <div class="text-amber-600 pl-2.5 pt-1.5 xl:text-4xl">${tour.country}</div>
+                    <div class="text-current mb-2.5 pl-2.5 xl:text-2xl">${tour.city}</div>
                 </div>
             </div>
-        </div>
+
+            <div class="flex flex-col info border drop-shadow-lg xl:mx-10 my-10">
+            
+                <div class="font-basic text-sky-600 text-center font-semibold px-2 xl:text-2xl pt-6 pb-6">${tour.hotelName}</div>
+            
+                <div class="pl-6">
+                    <div class="font-basic text-current pb-2 xl:text-xl pt-6>${tour.startTime}</div>
+                    <div class="font-basic text-current xl:text-xl pb-6">${tour.endTime}</div>
+                </div>
+            
+                <div class="flex flex-col pb-10 pt-6 px-6">
+                    <div class="flex">
+                        <img src="/images/icon-price.png" class="w-12 h-12">
+                        <div class="font-basic text-rose-700 pt-6  pl-2 xl:text-3xl font-bold">${tour.price}</div> 
+                        <p class="font-basic text-rose-700 pt-9 pl-2 xl:text-base">рублей</p>
+                    </div>
+
+                    <div class="flex">
+                        <img src="/images/icon-rating.png" class="w-12 h-12">
+                        <div class="font-basic text-amber-500 pt-4 pl-2 xl:text-2xl font-medium">${tour.rating}</div> 
+                        <p class="font-basic text-amber-500 pt-6 pl-2 xl:text-base">по версии TopHotels.com</p>
+                    </div>
+                </div>
+            </div>
     </div>
         `
     });
+
+  
 
 }
 
