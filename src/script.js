8c7321f5ece52b07d0c94c3c881f04296884714c
carpediem511 <vampire.0514@gmail.com>
2023-03-04 15:06:13 +0300

    не знаю, что наделала. надеюсь работает

--- src/script.js
+++ src/script.js
@@ -83,13 +83,13 @@ function renderTours(tours) {
                     <div class="flex justify-center pt-6 max-h-6">
                         <img class="px-8 w-full h-full object-center max-h-48 sm:max-h-54 md:max-h-28 lg:max-h-32 xl:max-h-48" src="${tour.image}">
                     </div>
-
+    
                     <div class="title flex flex-col absolute font-attention">
                         <div class="text-amber-600 pl-2.5 pt-1.5 xl:text-4xl">${tour.country}</div>
                         <div class="text-current mb-2.5 pl-2.5 xl:text-2xl" id="cityId">${city}</div>
                     </div>
                 </div>
-
+    
                 <div class="flex flex-col info border drop-shadow-lg xl:mx-10 my-10">
                 
                     <div class="hotel font-basic text-sky-600 text-center font-semibold px-2 xl:text-2xl pt-6 pb-6">${tour.hotelName}</div>
@@ -101,14 +101,14 @@ function renderTours(tours) {
                     продолжительность:</span> ${duration} дней
                     </div>
                     </div>
-
+                
                     <div class="flex flex-col pb-10 pt-6 px-2 xl:px-10">
                         <div class="flex">
                             <img src="/images/icon-price.png" class="w-12 h-12">
                             <div class="font-basic text-rose-700 pt-6  pl-2 xl:text-3xl font-bold">${tour.price}</div> 
                             <p class="font-basic text-rose-700 pt-9 pl-2 xl:text-base">рублей</p>
                         </div>
-
+    
                         <div class="flex">
                             <img src="/images/icon-rating.png" class="w-12 h-12">
                             <div class="font-basic text-amber-500 pt-4 pl-2 xl:text-2xl font-medium" id="rating">${tour.rating}</div> 
@@ -309,41 +309,6 @@ function emailTest(input) {
   return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,20})+$/.test(input.value)
 }
 
-function saveToLocalStorage() {
-    const toursJson = JSON.stringify(favoritTours)
-    localStorage.setItem("favoritTours", toursJson)
-}
-
-function deleteFavoriteTour(id) {
-    let fav = favoritTours.find(f => f.id === id)
-    let index = favoritTours.indexOf(fav)
-    favoritTours.splice(index, 1)
-    saveToLocalStorage()
-}
-document.getElementById('allTours').addEventListener('click', () => renderTours(tours))
-document.getElementById('favoritTours').addEventListener('click', () => renderFavoriteTours(favoritTours))
-
-function renderFavoriteTours(favoritTours) {
-    if(favoritTours.length === 0) {
-        document.getElementById("tours-all").innerHTML =
-        '<div><img src="/images/icon-sad_smile.png" class="oups"> <div class="nothing">У Вас нет избранных туров!</div></div>'
-    } else {
-        renderTours(favoritTours)
-    }
-
-}
-
-function checkFavoritTours() {
-
-    if (favoritTours.length === 0) {
-        document.getElementById("tours-all").innerHTML =
-        '<div><img src="/images/icon-sad_smile.png" class="oups"> <div class="nothing">У Вас нет избранных туров!</div></div>'
-    }
-}
-
-
-
-
 document.getElementById("countriesFilter").addEventListener("change", () => filterByCountry(tours))
 
 function filterByCountry(tours) {
@@ -530,6 +495,5 @@ if (toursJson) {
 tours = JSON.parse(toursJson);
 } 
 
-checkFavoritTours()
 getData()
 init()
